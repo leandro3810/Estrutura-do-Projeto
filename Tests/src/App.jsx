28ae7305b9857d604375bd0b8c1993355a2e2394
Leandro Ribeiro Gomes <ledielsong9@gmail.com>
2025-05-27 14:48:16 -0300

    Create App.jsx
    Signed-off-by: Leandro Ribeiro Gomes <ledielsong9@gmail.com>

---
+++ Tests/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DigitalClock from "./components/DigitalClock";
+
+export default function App() {
+  return (
+    <div>
+      <h1>World Clock Dashboard</h1>
+      <DigitalClock />
+    </div>
+  );
+}
